a7456cad5620d2eaf5737106d1da1741085bff7a
adensmith <adensmith@adens-air.unl.edu>
2024-08-30 10:53:11 -0500

    adens commit

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 
 	public static void main(String args[]) {
 		System.out.println("Liam Makela");
+		System.out.println("Aden Smith");
+		System.out.println("We are computer science majors.");
 	}
 
 
